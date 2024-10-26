741a455e863141e62b6167b9f50b8155130bd25b
Pierre Favier <pierrefavier26@gmail.com>
2024-10-26 13:06:00 +0200

    exo eau 00

--- eau00.py
+++ eau00.py
@@ -1,1 +1,7 @@
-#test
+numbers = list(range(0, 1000))
+string_number = [f'{number:03}' for number in numbers]
+
+for i in string_number:
+    if i[0] != i[1] and i[0] != i[2] and i[1] != i[2]:
+        print(i, end=", ")
+
